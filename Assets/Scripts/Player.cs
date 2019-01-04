41687ca93b6ce52a5eb90ef96193291f07b25ec6
Ray Kuo <ray.kuo@sap.com>
2019-01-04 15:16:43 -0800

    use easy way to move player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,97 +3,84 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	public float speed = 10f;
-	public Vector2 maxVelocity = new Vector2(3, 5);
-	public bool standing;
-	public float jetSpeed = 15f;
-	public float airSpeedMultiplier = .3f;
-	public AudioClip leftFootSound;
-	public AudioClip rightFootSound;
-	public AudioClip thudSound;
-	public AudioClip rocketSound;
+    [HideInInspector] public bool facingRight = true;
+    [HideInInspector] public bool jump = false;
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+    public float jumpForce = 1000f;
+    public Transform groundCheck;
 
-	private Animator animator;
-	private PlayerController controller;
 
-	void Start(){
-		controller = GetComponent<PlayerController> ();
-		animator = GetComponent<Animator> ();
-	}
+    private bool grounded = false;
+    private Animator animator;
+    private Rigidbody2D rb2d;
 
-	void PlayLeftFootSound(){
-		if (leftFootSound)
-			AudioSource.PlayClipAtPoint (leftFootSound, transform.position);
-	}
 
-	void PlayRightFootSound(){
-		if (rightFootSound)
-			AudioSource.PlayClipAtPoint (rightFootSound, transform.position);
-	}
+    // Use this for initialization
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
 
-	void PlayRocketSound(){
-		if (!rocketSound || GameObject.Find ("RocketSound"))
-			return;
+    // Update is called once per frame
+    void Update()
+    {
+        var absVelX = Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x);
+        var absVelY = Mathf.Abs(GetComponent<Rigidbody2D>().velocity.y);
 
-		GameObject go = new GameObject ("RocketSound");
-		AudioSource aSrc = go.AddComponent<AudioSource> ();
-		aSrc.clip = rocketSound;
-		aSrc.volume = 0.7f;
-		aSrc.Play ();
+        if (absVelY < .2f)
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
 
-		Destroy (go, rocketSound.length);
-
-	}
-
-	void OnCollisionEnter2D(Collision2D target){
-		if (!standing) {
-			var absVelX = Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x);
-			var absVelY = Mathf.Abs(GetComponent<Rigidbody2D>().velocity.y);
-
-			if(absVelX <= .1f || absVelY <= .1f){
-				if(thudSound)
-					AudioSource.PlayClipAtPoint(thudSound, transform.position);
-			}
-		}
-
-	}
-
-	// Update is called once per frame
-	void Update () {
-		var forceX = 0f;
-		var forceY = 0f;
-
-		var absVelX = Mathf.Abs (GetComponent<Rigidbody2D>().velocity.x);
-		var absVelY = Mathf.Abs (GetComponent<Rigidbody2D>().velocity.y);
-
-		if (absVelY < .2f) {
-			standing = true;
-		} else {
-			standing = false;
-		}
-
-		if (controller.moving.x != 0) {
-			if (absVelX < maxVelocity.x) {
-
-				forceX = standing ? speed * controller.moving.x : (speed * controller.moving.x * airSpeedMultiplier);
-
-				transform.localScale = new Vector3 (forceX > 0 ? 1 : -1, 1, 1);
-			}
-            animator.SetTrigger("Walk");
-		} else {
+        if (absVelX < .2f) {
             animator.SetTrigger("Idle");
-		}
+        } else {
+            animator.SetTrigger("Walk");
+        }
 
-		if (controller.moving.y > 0) {
-			PlayRocketSound();
-						if (absVelY < maxVelocity.y)
-								forceY = jetSpeed * controller.moving.y;
+        //grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
 
-						animator.SetInteger ("AnimState", 2);
-				} else if (absVelY > 0) {
-			animator.SetInteger("AnimState", 3);
-				}
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+        {
+            jump = true;
+        }
+    }
 
-		GetComponent<Rigidbody2D>().AddForce (new Vector2 (forceX, forceY));
-	}
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        if (h * rb2d.velocity.x < maxSpeed)
+            rb2d.AddForce(Vector2.right * h * moveForce);
+
+        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+
+        if (h > 0 && !facingRight)
+            Flip();
+        else if (h < 0 && facingRight)
+            Flip();
+
+        if (jump)
+        {
+            //anim.SetTrigger("Jump");
+            rb2d.AddForce(new Vector2(0f, jumpForce));
+            jump = false;
+        }
+    }
+
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
